176693b54c75eb419cb959724f95e6215b43cd5c
DanielHauge <Animcuil@gmail.com>
2018-05-16 23:04:51 +0200

    some small changes -> maybe fix

--- DBParadigmsGroup10/src/main/java/Main/WebInitializer.java
+++ DBParadigmsGroup10/src/main/java/Main/WebInitializer.java
@@ -1,11 +1,18 @@
+
+
 package Main;
 
+import DataAcessors.RedisDataAcessor;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
 
 public class WebInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        Main.DA = new RedisDataAcessor(System.getenv("DBIP"));
         return application.sources(Main.class);
     }
 }
+
