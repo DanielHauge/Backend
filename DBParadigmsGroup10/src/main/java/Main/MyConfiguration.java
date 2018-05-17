9465ab2c7f13469513fadd082862af0ce2bc6991
Daniel Hauge <Animcuil@gmail.com>
2018-05-17 12:27:23 +0200

    typo

--- DBParadigmsGroup10/src/main/java/Main/MyConfiguration.java
+++ DBParadigmsGroup10/src/main/java/Main/MyConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class MyConfiguration {
-t 
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
