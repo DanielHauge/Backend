6032c043d54f325a4a5b92bb9f04a594c6338243
Daniel Hauge <Animcuil@gmail.com>
2018-05-17 16:32:12 +0200

    change

--- DBParadigmsGroup10/src/main/java/DataObjects/CityWithCords.java
+++ DBParadigmsGroup10/src/main/java/DataObjects/CityWithCords.java
@@ -6,13 +6,13 @@ import DataObjects.Json.DataSerializer;
 public class CityWithCords extends DataSerializer implements DataObject {
 
     public final String cityName;
-    public final double latitude;
-    public final double longitude;
+    public final double lat;
+    public final double lng;
 
     public CityWithCords(String name, double lat, double lon){
         this.cityName = name;
-        this.latitude = lat;
-        this.longitude = lon;
+        this.lat = lat;
+        this.lng = lon;
     }
 
 
