d1ecdee45a0edb2f34af2d4c895505fdb771be77
Daniel Hauge <Animcuil@gmail.com>
2018-05-17 12:26:11 +0200

    some cors try

--- DBParadigmsGroup10/src/main/java/Main/Main.java
+++ DBParadigmsGroup10/src/main/java/Main/Main.java
@@ -12,8 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
-@ComponentScan
 @EnableAutoConfiguration
 public class Main {
 
